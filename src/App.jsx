ace520cd49a7bffea287ce72b871740ffdb55f78
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-05 18:06:55 +0300

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import './App.css';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+
         <Switch>
-          <Route path="/" exact />
+          <Route path="/" exact>
+            <Home />
+          </Route>
         </Switch>
       </Router>
     </div>
