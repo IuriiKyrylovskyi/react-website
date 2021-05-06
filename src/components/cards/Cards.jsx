7197736158b6fe07a0616d8952e4871ac2574443
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-06 17:56:29 +0300

    CardItem

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -2,6 +2,44 @@ import React from 'react';
 import CardItem from '../cardItem/CardItem';
 import './cards.scss';
 
+const cardItems = [
+  {
+    id: '1',
+    src: 'images/img-9.jpg',
+    text: 'Explore the hidden waterfall deep inside the Amazon Jungle',
+    label: 'Advanture',
+    path: '/services',
+  },
+  {
+    id: '2',
+    src: 'images/img-2.jpg',
+    text: 'Travel through the Island of Bali in a Private Cruise',
+    label: 'Luxury',
+    path: '/services',
+  },
+  {
+    id: '3',
+    src: 'images/img-3.jpg',
+    text: 'Set Sail in the Atlantic Ocean visitng Uncharted Waters',
+    label: 'Mystery',
+    path: '/services',
+  },
+  {
+    id: '4',
+    src: 'images/img-4.jpg',
+    text: 'Experience Football on Top of the Himilayan Mountains',
+    label: 'Advanture',
+    path: '/products',
+  },
+  {
+    id: '5',
+    src: 'images/img-8.jpg',
+    text: 'Ride through the Sahara Desert on a guided camel tour',
+    label: 'Adrenaline',
+    path: '/sign-up',
+  },
+];
+
 function Cards() {
   return (
     <div className="cards">
@@ -9,12 +47,17 @@ function Cards() {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem
-              src="images/img-9.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Jungle"
-              label="Advanture"
-              path="/services"
-            />
+            {cardItems.length > 0 &&
+              cardItems.map(item => {
+                const {id, src, text, label, path} = item;
+                return <CardItem key={id}
+                  src={src}
+                  text={text}
+                  label={label}
+                  path={path}
+                />
+              })
+            }
           </ul>
         </div>
       </div>
